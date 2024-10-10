9275cb110b0c2ea10232ac7bbff25c41cec6e199
momen-alshouha <momenalshouha@outlook.com>
2024-10-10 12:26:19 +0300

    Add project files.

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(asp.net_vidly.Startup))]
+namespace asp.net_vidly
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
