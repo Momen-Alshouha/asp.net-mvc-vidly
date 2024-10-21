02a704cbf18e5611844e7b6e15e7cd6e0932d925
momen-alshouha <momenalshouha@outlook.com>
2024-10-21 23:24:33 +0300

    feat: Enforce global authorization and allow anonymous access to HomeController
    - Added global authorization to restrict access to authenticated users
    - Applied [AllowAnonymous] to HomeController to allow public access to home, about, and contact pages

--- App_Start/FilterConfig.cs
+++ App_Start/FilterConfig.cs
@@ -8,6 +8,7 @@ namespace asp.net_vidly
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
+            filters.Add(new AuthorizeAttribute());
         }
     }
 }
--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using System.Web.Mvc;
 
 namespace asp.net_vidly.Controllers
 {
+    [AllowAnonymous]
     public class HomeController : Controller
     {
         public ActionResult Index()
